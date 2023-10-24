d7f0f3b468ee00b5f1128312e770eec2c7b40929
Rohit Tandon <tandonrohit0411@gmail.com>
2023-10-24 20:37:03 +0530

    Updates

---
+++ precache-manifest.a20e9b4f52303bd9daab1431bbe60139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5675696efd2c3aaf033c71a36cbd3579",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "b1ee7462f3d801707dc5",
+    "url": "/pokemon/static/css/main.fae8d5f7.chunk.css"
+  },
+  {
+    "revision": "e2ecbaf2242f7b738644",
+    "url": "/pokemon/static/js/2.fa45d356.chunk.js"
+  },
+  {
+    "revision": "eeb29d83d1bcbaba4e91",
+    "url": "/pokemon/static/js/3.82db40f9.chunk.js"
+  },
+  {
+    "revision": "b1ee7462f3d801707dc5",
+    "url": "/pokemon/static/js/main.f9c5ae98.chunk.js"
+  },
+  {
+    "revision": "6d17c158e2a5f6e21f97",
+    "url": "/pokemon/static/js/runtime~main.0d1990f4.js"
+  }
+]);
